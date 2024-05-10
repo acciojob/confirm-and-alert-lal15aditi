4bc1a56955ae6a5b720aed10f80168a61406231b
Aditi Lal <139861603+lal15aditi@users.noreply.github.com>
2024-05-10 22:22:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+// Ask for confirmation
+var confirmation = confirm("Do you want to proceed?");
+
+// Check user's choice
+if (confirmation) {
+    // If OK is clicked
+    alert("You clicked OK. Proceeding...");
+} else {
+    // If Cancel is clicked
+    alert("You clicked Cancel. Exiting...");
+}
